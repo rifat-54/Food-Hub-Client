7ed842b173859a9aaf0bf2320232d19c9376a8a0
rifat <rifat999por@gmail.com>
2026-06-22 12:39:42 +0600

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>home page</h1>
+      <Button>click hrere</Button>
     </div>
   );
 }
