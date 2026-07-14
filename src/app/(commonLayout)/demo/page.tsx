7e94f003f0d2fb44e8f91e31ba449b0feaf63fd2
rifat <rifat999por@gmail.com>
2026-07-14 21:42:51 +0600

    debbing 39

--- src/app/(commonLayout)/demo/page.tsx
+++ src/app/(commonLayout)/demo/page.tsx
@@ -2,22 +2,40 @@
 
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const url = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Demo() {
   const router = useRouter();
 
-  const { data, isPending } = authClient.useSession();
+  const { data: session, isPending } = authClient.useSession();
+
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    if (!isPending && !data) {
+    if (!isPending && !session) {
       router.replace("/login");
+      return;
     }
-  }, [data, isPending, router]);
+
+    async function getUser() {
+      const res = await fetch(`${url}/user/me`, {
+        credentials: "include",
+      });
+
+      const result = await res.json();
+      setUser(result);
+    }
+
+    if (session) {
+      getUser();
+    }
+  }, [session, isPending, router]);
 
   if (isPending) {
     return <div>Loading...</div>;
   }
 
-  return <div>Demo</div>;
+  return <div>{JSON.stringify(user)}</div>;
 }
